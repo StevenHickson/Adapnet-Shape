7da85655cde8ae2d8a2cb55ac1d094e9843fea42
Steven Hickson <me@stevenhickson.com>
2019-07-24 19:52:28 -0400

    Updated working training and plots for tensorboard (colormap not working yet)

--- train.py
+++ train.py
@@ -21,9 +21,55 @@ import re
 import tensorflow as tf
 import yaml
 from dataset.helper import *
+import matplotlib
+import matplotlib.cm
 
 PARSER = argparse.ArgumentParser()
 PARSER.add_argument('-c', '--config', default='config/cityscapes_train.config')
+
+def colorize(value, vmin=None, vmax=None, cmap=None):
+    """
+    A utility function for TensorFlow that maps a grayscale image to a matplotlib
+    colormap for use with TensorBoard image summaries.
+    By default it will normalize the input value to the range 0..1 before mapping
+    to a grayscale colormap.
+    Arguments:
+      - value: 2D Tensor of shape [height, width] or 3D Tensor of shape
+        [height, width, 1].
+      - vmin: the minimum value of the range used for normalization.
+        (Default: value minimum)
+      - vmax: the maximum value of the range used for normalization.
+        (Default: value maximum)
+      - cmap: a valid cmap named for use with matplotlib's `get_cmap`.
+        (Default: 'gray')
+    Example usage:
+    ```
+    output = tf.random_uniform(shape=[256, 256, 1])
+    output_color = colorize(output, vmin=0.0, vmax=1.0, cmap='viridis')
+    tf.summary.image('output', output_color)
+    ```
+    
+    Returns a 3D tensor of shape [height, width, 3].
+    """
+
+    # normalize
+    vmin = tf.reduce_min(value) if vmin is None else vmin
+    vmax = tf.reduce_max(value) if vmax is None else vmax
+    value = (value - vmin) / (vmax - vmin) # vmin..vmax
+
+    # squeeze last dim if it exists
+    value = tf.squeeze(value)
+
+    # quantize
+    indices = tf.to_int32(tf.round(value * 255))
+
+    # gather
+    cm = matplotlib.cm.get_cmap(cmap if cmap is not None else 'gray')
+    colors = cm(np.arange(256))[:, :3]
+    colors = tf.constant(colors, dtype=tf.float32)
+    value = tf.gather(colors, indices) * 255
+
+    return value
 
 def train_func(config):
     os.environ['CUDA_VISIBLE_DEVICES'] = config['gpu_id']
@@ -43,8 +89,8 @@ def train_func(config):
         model.build_graph(images_pl, labels_pl)
         model.create_optimizer()
         tf.summary.image('rgb', images_pl)
-        tf.summary.image('label', tf.expand_dims(tf.cast(tf.math.argmax(labels_pl, axis=-1), tf.uint8), axis=-1))
-        tf.summary.image('estimate',  tf.expand_dims(tf.cast(tf.math.argmax(model.softmax, axis=-1), tf.uint8), axis=-1))
+        tf.summary.image('label', tf.cast(colorize(tf.math.argmax(labels_pl, axis=-1), cmap='jet'), tf.uint8))
+        tf.summary.image('estimate',  tf.cast(colorize(tf.math.argmax(model.softmax, axis=-1), cmap='jet'), tf.uint8))
         model._create_summaries()
  
     config1 = tf.ConfigProto()
