c48ff9267545837f0948ca332ef107afff85191a
Steven Hickson <shickson3@bb8.cc.gatech.edu>
2019-08-13 16:14:36 -0400

    Added port number as argument for tensorboard launch script

--- launch_tensorboard.py
+++ launch_tensorboard.py
@@ -1,8 +1,9 @@
 from tensorboard import program
 import time
+import sys
 
 tb = program.TensorBoard()
-tb.configure(argv=[None, '--logdir', './', '--port', '9997'])
+tb.configure(argv=[None, '--logdir', './', '--port', sys.argv[1]])
 tb.launch()
 while(1):
     time.sleep(1)
