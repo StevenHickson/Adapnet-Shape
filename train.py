49929897972bb98b56b5e267fc3e2c99913d0bd6
StevenHickson <me@stevenhickson.com>
2019-08-14 17:16:18 -0400

    Attempted combining to one training script

--- train.py
+++ train.py
@@ -25,6 +25,41 @@ from train_utils import *
 PARSER = argparse.ArgumentParser()
 PARSER.add_argument('-c', '--config', default='config/cityscapes_train.config')
 
+def setup_model(model, config):
+    if config['input_modality'] == 'rgb':
+        images_pl = tf.placeholder(tf.float32, [None, config['height'], config['width'], 3])
+    elif config['input_modality'] == 'normals':
+        images_pl = tf.placeholder(tf.float32, [None, config['height'], config['width'], 3])
+    elif config['input_modality'] == 'depth':
+        images_pl = tf.placeholder(tf.uint16, [None, config['height'], config['width'], 1])
+        images_pl = tf.cast(images_pl, tf.float32)
+    if config['output_modality'] == 'labels':
+        labels_pl = tf.placeholder(tf.float32, [None, config['height'], config['width'],
+                                                config['num_classes']])
+        depths_pl = None
+        weights = None
+    elif config['output_modality'] == 'normals':
+        labels_pl = tf.placeholder(tf.float32, [None, config['height'], config['width'], 3])
+        depths_pl = tf.placeholder(tf.uint16, [None, config['height'], config['width'], 1])
+        weights = tf.cast(tf.math.not_equal(tf.cast(depths_pl, tf.float32), 0), tf.float32)
+    
+    model.build_graph(images_pl, labels_pl, weights)
+    model.create_optimizer()
+    
+    if config['output_modality'] == 'labels':
+        labels_argmax = extract_labels(labels_pl)
+        estimate_argmax = extract_labels(model.softmax)
+        add_image_summaries(images=images_pl, labels=labels_argmax, labels_estimate=estimate_argmax)
+        update_ops = add_metric_summaries(images=images_pl, labels=labels_argmax, labels_estimate=estimate_argmax, num_classes=config['num_classes'])
+    elif config['output_modality'] == 'normals':
+        label = extract_normals(labels_pl)
+        pred = extract_normals(model_output)
+        add_image_summaries(images=images_pl, normals=label, normals_estimate=pred)
+        update_ops = add_metric_summaries(images=images_pl, normals=label, normals_estimate=pred, depth_weights=weights, config=config)
+    
+    model._create_summaries()
+    return image_pl, depths_pl, labels_pl, update_ops
+
 def train_func(config):
     #os.environ['CUDA_VISIBLE_DEVICES'] = config['gpu_id']
     module = importlib.import_module('models.'+config['model'])
@@ -35,26 +70,18 @@ def train_func(config):
     resnet_name = 'resnet_v2_50'
     global_step = tf.train.get_or_create_global_step()
     step = 0
+    compute_normals = (config['output_modality'] == 'normals')
 
     with tf.variable_scope(resnet_name):
         model = model_func(num_classes=config['num_classes'], learning_rate=config['learning_rate'],
                            decay_steps=config['max_iteration'], power=config['power'],
-                           global_step=global_step)
-        images_pl = tf.placeholder(tf.float32, [None, config['height'], config['width'], 3])
-        labels_pl = tf.placeholder(tf.float32, [None, config['height'], config['width'],
-                                                config['num_classes']])
-        model.build_graph(images_pl, labels_pl)
-        model.create_optimizer()
-        labels_argmax = extract_labels(labels_pl)
-        estimate_argmax = extract_labels(model.softmax)
-        add_image_summaries(images=images_pl, labels=labels_argmax, labels_estimate=estimate_argmax)
-        update_ops = add_metric_summaries(images=images_pl, labels=labels_argmax, labels_estimate=estimate_argmax, num_classes=config['num_classes'])
-        model._create_summaries()
+                           global_step=global_step, compute_normals=compute_normals)
+        image_pl, depths_pl, labels_pl, update_ops = setup_model(model, config)
  
     config1 = tf.ConfigProto()
     config1.gpu_options.allow_growth = True
     sess = tf.Session(config=config1)
-    writer = tf.summary.FileWriter('./graphs', sess.graph)
+    writer = tf.summary.FileWriter(config['summary_dir'], sess.graph)
     sess.run(tf.global_variables_initializer())
     sess.run(tf.local_variables_initializer())
     total_loss = 0.0
@@ -86,8 +113,20 @@ def train_func(config):
        
     while 1:
         try:
-            img, label = sess.run([data_list[0], data_list[3]])
-            feed_dict = {images_pl: img, labels_pl: label}
+            if config['input_modality'] == 'rgb':
+                net_input = data_list[0]
+            elif config['input_modality'] == 'depth':
+                net_input = data_list[1]
+            elif config['input_modality'] == 'normals':
+                net_input = data_list[2]
+
+            if config['output_modality'] == 'labels':
+                img, label = sess.run([net_input, data_list[3]])
+                feed_dict = {images_pl: img, labels_pl: label}
+            elif config['output_modality'] == 'normals':
+                img, depth, normals = sess.run([net_input, data_list[1], data_list[2]])
+                feed_dict = {images_pl: img, depths_pl: depth, labels_pl: normals}
+
             inputs = [model.loss, model.train_op, model.summary_op] + update_ops
             result = sess.run(inputs, feed_dict=feed_dict)
             loss_batch = result[0]
