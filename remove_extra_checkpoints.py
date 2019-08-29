d8dba71a62506c2d74deaf07d3ce8ea605e68826
Steven Hickson <shickson3@asimo.cc.gatech.edu>
2019-08-29 15:21:06 -0400

    Added file to remove spurious checkpoints to save space

---
+++ remove_extra_checkpoints.py
@@ -0,0 +1,17 @@
+import os
+import sys
+
+valid_checkpoints = [19999, 39999, 59999, 79999, 99999, 119999, 139999, 150000]
+
+#ex model.ckpt-81999.data-00000-of-00001
+
+for filename in os.listdir(sys.argv[1]):
+    try:
+        # Do this to throw errors for thinks that don't have model.ckpt
+        no_op = filename.split('model')[1]
+        file_num = int(filename.split('-')[1].split('.')[0])
+        if file_num not in valid_checkpoints:
+            os.remove(os.path.join(sys.argv[1],filename))
+    except:
+        print('Ignoring ' + filename)
+
