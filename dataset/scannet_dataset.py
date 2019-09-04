5d99f61b987b6cf1f80511db9462cfe5465b1abe
Steven Hickson <me@stevenhickson.com>
2019-09-04 12:14:00 -0400

    Fixing scannet dataset bug

--- dataset/scannet_dataset.py
+++ dataset/scannet_dataset.py
@@ -1,6 +1,7 @@
 from helper import DatasetHelper
 import numpy as np
 import sys
+import csv
 sys.path.append('/nethome/shickson3/CreateNormals/')
 from python.calc_normals import NormalCalculation
 
@@ -9,6 +10,7 @@ normal_params = [5,0.02,30,0.04]
 flat_labels = [1,2,7,8,9,11,19,22,24,29,30,32]
 
 convert_to_20 = [0,1,2,3,4,5,6,7,8,9,10,11,12,9,13,20,14,20,4,20,2,0,0,0,15,20,0,0,16,0,20,0,20,17,18,20,19,0,20,20,0]
+label_nyu_mapping = dict()
 label_nyu_mapping[0] = 0
 with open('/srv/datasets/scannet/scannetv2-labels.combined.tsv') as tsvfile:
     reader = csv.reader(tsvfile, delimiter='\t')
