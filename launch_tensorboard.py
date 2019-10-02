4f6b4812620134cf53241d0915ee510cc81660eb
Steven Hickson <me@stevenhickson.com>
2019-10-02 12:25:54 -0400

    Updated tensorboard to allow specific directories

--- launch_tensorboard.py
+++ launch_tensorboard.py
@@ -4,7 +4,7 @@ import sys
 
 tb = program.TensorBoard()
 print('Launching tensorboard on port ' + str(sys.argv[1]))
-tb.configure(argv=[None, '--logdir', './', '--port', sys.argv[1]])
+tb.configure(argv=[None, '--logdir', './graphs/' + str(sys.argv[2]), '--port', sys.argv[1]])
 tb.launch()
 while(1):
     time.sleep(1)
