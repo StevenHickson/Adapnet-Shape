b5eeba6b724e40dbbd99c324b2e9a8d68403737d
Steven Hickson <me@stevenhickson.com>
2019-10-24 16:27:19 -0400

    Making remove_extra_checkpoints work for whole directories not each one individually

--- remove_extra_checkpoints.py
+++ remove_extra_checkpoints.py
@@ -5,13 +5,14 @@ valid_checkpoints = [49999, 99999, 150000]
 
 #ex model.ckpt-81999.data-00000-of-00001
 
-for filename in os.listdir(sys.argv[1]):
+for (dirpath, dirname, filenames) in os.walk(sys.argv[1]):
     try:
         # Do this to throw errors for thinks that don't have model.ckpt
-        no_op = filename.split('model')[1]
-        file_num = int(filename.split('-')[1].split('.')[0])
-        if file_num not in valid_checkpoints:
-            os.remove(os.path.join(sys.argv[1],filename))
+        for filename in filenames:
+            no_op = filename.split('model')[1]
+            file_num = int(filename.split('-')[1].split('.')[0])
+            if file_num not in valid_checkpoints:
+                os.remove(os.path.join(dirpath,filename))
+                print('removing filename ' + os.path.join(dirpath,filename))
     except:
-        print('Ignoring ' + filename)
-
+        pass
